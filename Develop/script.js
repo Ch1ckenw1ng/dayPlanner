066ba8842ba849c39f02cb741921037e73e7d9ac
ch1ckenw1ng <vargad4876@gmail.com>
2021-03-25 23:36:18 -0500

    basic structure of java plan

--- Develop/script.js
+++ Develop/script.js
@@ -3,6 +3,17 @@ let container = document.getElementsByTagName("lead");
 let time = moment().hours();
 let date = moment().format("dddd, MMMM,  h:mm a");
 let currentDate = document.querySelector("#lead");
+let hour = moment().format("h");
+
+let t1 = document.getElementById("t1");
+let t2 = document.getElementById("t2");
+let t3 = document.getElementById("t3");
+let t4 = document.getElementById("t4");
+let t5 = document.getElementById("t5");
+let t6 = document.getElementById("t6");
+let t7 = document.getElementById("t7");
+let t8 = document.getElementById("t8");
+let t9 = document.getElementById("t9");
 
 //document.getElementById("currentday").textContent = "today is " + date;
 //currentDate.appendChild.textcontent(date);
@@ -10,4 +21,9 @@ currentDay.innerHTML = date;
 
 console.log(time);
 console.log(date);
+console.log(hour);
 
+if (t1 = )
+
+
+t1.setAttribute("style", "background-color: red;" );

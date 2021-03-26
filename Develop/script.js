c2f76dd0f8690c310fe6363ddd931cd6d08ab74c
ch1ckenw1ng <vargad4876@gmail.com>
2021-03-25 22:27:55 -0500

    got the concept of time working

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,17 @@
 let container = document.getElementsByTagName("div");
+let time = moment().hours();
+let date = moment().format("dddd, MMMM");
+let currentDate = document.querySelector("#lead");
+
+//document.getElementById("currentday").textContent = "today is " + date;
+//currentDate.appendChild.textcontent(date);
 
 setInterval(() => {
     
     let today = moment();
-    $("container").text(today.format("MMMM Do YYYY, h:mm:ss a"));
+    $("div").text(today.format("h:mm:ss a"));
 }, 1000);
+
+console.log(time);
+console.log(date);
+
